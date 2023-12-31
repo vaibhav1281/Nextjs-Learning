be21097eab4d0cea74c136f78c0502158580573e
vaibhav1281 <vaibhav1281saroj@gmail.com>
2023-12-31 17:41:18 +0530

    Added Search Functionality In Invoice

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,8 +1,29 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 
 export default function Search({ placeholder }: { placeholder: string }) {
+
+  // Updating the URL with the search params
+  const searchParams = useSearchParams()
+
+  // update the URL using 'useRouter' and 'usePathname' hooks
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  function handleSearch(term: string){
+    // console.log(`Searching... ${term}`);
+    const params = new URLSearchParams(searchParams)
+
+    if (term) {
+      params.set('query', term);
+    } else {
+      params.delete('query');
+    }
+    replace(`${pathname}?${params.toString()}`);
+  }
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -11,6 +32,13 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+
+        // Capture the user's input.
+        onChange={(e) => {
+          handleSearch(e.target.value);
+        }}
+        // Keeping the URL and input in sync
+        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
